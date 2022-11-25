8561c1764eb7dbc7a5fa7d2183f82b87cc99be72
Tarek Noubli <tareknoubli@gmail.com>
2022-11-25 13:48:59 +0100

    Update sauce.js
    add multer and auth in router logic

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,15 +1,15 @@
 const express = require('express');
-
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
 const router = express.Router();
-
 const Sauce = require('../models/Sauce');
 const sauceController = require('../controllers/sauce')
 
-router.get('/', sauceController.getAllSauces);
-router.get('/:id', sauceController.getOneSauce);
-router.post('/', sauceController.createSauce);
-router.put('/:id', sauceController.modifySauce);
-router.delete('/:id', sauceController.deleteSauce);
-router.post('/:id/like', sauceController.likeSauce);
+router.get('/', auth, sauceController.getAllSauces);
+router.get('/:id', auth, sauceController.getOneSauce);
+router.post('/', auth, multer, sauceController.createSauce);
+router.put('/:id', auth, multer, sauceController.modifySauce);
+router.delete('/:id', auth, sauceController.deleteSauce);
+router.post('/:id/like', auth, sauceController.likeSauce);
 
 module.exports = router;
