6b242b065780f8d3447d4665fd8091eea4b25f96
Tarek Noubli <tareknoubli@gmail.com>
2022-11-29 16:29:14 +0100

    Update sauce.js
    removal of const Sauce model not used in the file

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,7 +2,6 @@ const express = require('express');
 const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config')
 const router = express.Router();
-const Sauce = require('../models/Sauce');
 const sauceController = require('../controllers/sauce')
 
 router.get('/', auth, sauceController.getAllSauces);
