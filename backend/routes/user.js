75c734421c6d7e61d042eeb7e9894a0d52ca6755
Tarek Noubli <tareknoubli@gmail.com>
2022-12-01 15:50:33 +0100

    Update user.js
    add middleware rateLimit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user')
+const userController = require('../controllers/user');
+const rateLimit = require('../middleware/rateLimiter');
 
-router.post('/signup', userController.signup)
-router.post('/login', userController.login)
+router.post('/signup', rateLimit, userController.signup);
+router.post('/login', rateLimit, userController.login);
 
 module.exports = router;
