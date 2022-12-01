db50cc987127bf0ed122ff7b57133c2d30343bd4
Tarek Noubli <tareknoubli@gmail.com>
2022-12-01 15:50:08 +0100

    Update sauce.js
    add rateLimit middleware on every router

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config')
+const rateLimit = require('../middleware/rateLimiter')
 const router = express.Router();
 const sauceController = require('../controllers/sauce')
 
-router.get('/', auth, sauceController.getAllSauces);
-router.get('/:id', auth, sauceController.getOneSauce);
-router.post('/', auth, multer, sauceController.createSauce);
-router.put('/:id', auth, multer, sauceController.modifySauce);
-router.delete('/:id', auth, sauceController.deleteSauce);
-router.post('/:id/like', auth, sauceController.likeSauce);
+router.get('/', rateLimit, auth, sauceController.getAllSauces);
+router.get('/:id', rateLimit, auth, sauceController.getOneSauce);
+router.post('/', rateLimit, auth, multer, sauceController.createSauce);
+router.put('/:id', rateLimit, auth, multer, sauceController.modifySauce);
+router.delete('/:id', rateLimit, auth, sauceController.deleteSauce);
+router.post('/:id/like', rateLimit, auth, sauceController.likeSauce);
 
 module.exports = router;
