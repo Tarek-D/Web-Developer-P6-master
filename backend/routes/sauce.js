df3901e431ef8f1a91841fef912aaab74ccb93db
Tarek Noubli <tareknoubli@gmail.com>
2022-12-02 14:51:06 +0100

    Rate Limit update
    removal rate limit in every routes because location is on main app file with app.use(rate limit)

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const auth = require('../middleware/auth')
-const multer = require('../middleware/multer-config')
-const rateLimit = require('../middleware/rateLimiter')
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 const router = express.Router();
-const sauceController = require('../controllers/sauce')
+const sauceController = require('../controllers/sauce');
 
-router.get('/', rateLimit, auth, sauceController.getAllSauces);
-router.get('/:id', rateLimit, auth, sauceController.getOneSauce);
-router.post('/', rateLimit, auth, multer, sauceController.createSauce);
-router.put('/:id', rateLimit, auth, multer, sauceController.modifySauce);
-router.delete('/:id', rateLimit, auth, sauceController.deleteSauce);
-router.post('/:id/like', rateLimit, auth, sauceController.likeSauce);
+router.get('/', auth, sauceController.getAllSauces);
+router.get('/:id', auth, sauceController.getOneSauce);
+router.post('/', auth, multer, sauceController.createSauce);
+router.put('/:id', auth, multer, sauceController.modifySauce);
+router.delete('/:id', auth, sauceController.deleteSauce);
+router.post('/:id/like', auth, sauceController.likeSauce);
 
 module.exports = router;
