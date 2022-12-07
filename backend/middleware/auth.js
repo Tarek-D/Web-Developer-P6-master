b0b9fd49069278ce591a3d50d872818bd8af4b05
Tarek Noubli <tareknoubli@gmail.com>
2022-12-07 15:14:58 +0100

    Update auth.js
    Replace hardwritten token by a environment variable

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config({ path : './config/.env' });
  
 module.exports = (req, res, next) => {
    try {
        const token = req.headers.authorization.split(' ')[1];
-       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // replace random token by environment variable
+       const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN); 
        const userId = decodedToken.userId;
        req.auth = {
            userId: userId
