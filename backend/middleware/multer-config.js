dfb35000955c378b07a738dfa737ebca12e96342
Tarek Noubli <tareknoubli@gmail.com>
2023-01-04 14:09:39 +0100

    Update multer-config.js
    add comments

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,11 +1,16 @@
+// library to handle HTTP requests that contain multipart/form-data, which is used for file uploads.
 const multer = require('multer');
 
+// object maps MIME types to file extensions.
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
 
+// specifies where to store uploaded files (in this case, the images directory) 
+// and how to name them (by concatenating the original file name, a timestamp, 
+// and the appropriate file extension).
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
@@ -17,4 +22,6 @@ const storage = multer.diskStorage({
   }
 });
 
+// function is called with the storage object as a configuration option. 
+// The .single('image') method tells multer to expect a single file with the field name 'image' to be uploaded.
 module.exports = multer({storage: storage}).single('image');
