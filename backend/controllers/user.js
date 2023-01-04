09f9d8af76e49416def3a39ed0072e51f48b4aa1
Tarek Noubli <tareknoubli@gmail.com>
2023-01-04 14:05:27 +0100

    Update user.js
    comments in english instead of french

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,7 +4,9 @@ const User = require('../models/User');
 require('dotenv').config({ path : './config/.env' });
 
 
-exports.signup = (req, res, next) => {
+exports.signup = (req, res) => {
+    // Notice that we don't check if user exist because front end is not able to handle this case.
+
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
@@ -12,22 +14,22 @@ exports.signup = (req, res, next) => {
                 password: hash
             });
             user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-                .catch(error => res.status(400).json({ error }));
+                .then(() => res.status(201).json({ message: 'User has been created in the database' }))
+                .catch(error => res.status(400).json({ error })); // If user exist and other cases
         })
         .catch(error => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {
+exports.login = (req, res) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                return res.status(401).json({ message: 'username or password failed' });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                        return res.status(401).json({ message: 'username or password failed' });
                     }
                     res.status(200).json({
                         userId: user._id,
