2fa0cfd12994d737195d819d39e70eb375973a11
Tarek Noubli <tareknoubli@gmail.com>
2023-01-04 14:07:29 +0100

    Update user.js
    add control password and control email middlewares signup route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
+const controlEmail = require('../middleware/controlEmail');
+const controlPassword = require('../middleware/controlPassword');
 
-router.post('/signup', userController.signup);
+router.post('/signup', controlPassword, controlEmail, userController.signup);
 router.post('/login', userController.login);
 
 module.exports = router;
